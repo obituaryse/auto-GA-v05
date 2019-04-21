d4d27b4fc10f079dae221b3988eabf8ca453a906
obituaryse <obituaryse@gmail.com>
2019-04-21 19:32:40 -0400

    tarea post

--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/PostManStepdefs.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/PostManStepdefs.java
@@ -3,6 +3,7 @@ package org.umssdiplo.automationv01.stepdefinitionproject;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.umssdiplo.automationv01.core.customrestassure.HandleRequest;
@@ -14,6 +15,7 @@ import static org.testng.Assert.assertEquals;
  */
 public class PostManStepdefs {
     private Response response;
+    private String resourcePost;
 
     @Given("^GET \"([^\"]*)\" postman endpoint is configured$")
     public void getPostmanEndpointIsConfigured(String headerEndpoint) throws Throwable {
@@ -27,13 +29,14 @@ public class PostManStepdefs {
 
     @Then("^the responde 'data' value is \"([^\"]*)\"$")
     public void theRespondeDataValueIs(String esperado) throws Throwable {
-        String actual = response.getDataValue();
-
-        Assert.assertEquals(esperado, actual);
+        response = HandleRequest.post(resourcePost,esperado);
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        String responseData = jsonPathEvaluator.get("data");
+        Assert.assertEquals(esperado,responseData,"Response body no contiene la data"+esperado);
     }
 
     @Given("^POST \"([^\"]*)\" postman endpoint is configured$")
-    public void postPostmanEndpointIsConfigured(String arg0) throws Throwable {
-
+    public void postPostmanEndpointIsConfigured(String headerEndpoint) throws Throwable {
+        resourcePost = headerEndpoint;
     }
 }
