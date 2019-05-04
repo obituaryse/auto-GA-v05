25a69ea9f05f697780d065af329bfa906fea23f3
Pamela Arnez <wen.pame04@gmail.com>
2019-05-04 03:12:12 -0400

    primer commit

--- src/main/java/org/umssdiplo/automationv01/core/customwebdriver/ManageDriver.java
+++ src/main/java/org/umssdiplo/automationv01/core/customwebdriver/ManageDriver.java
@@ -19,6 +19,8 @@ public class ManageDriver {
         BrowserType driverType = BrowserType.valueOf(PropertyAccessor.getInstance().getBrowser());
         webDriver = DriverFactory.getManageDriver(driverType);
         webDriver.manage().window().maximize();
+        restorePreviousTimeWait();
+        //this.setExplicitTimeWait(EXPLICIT_TIME_WAIT);
     }
 
     public static ManageDriver getInstance() {
